f242a80437fed92b251a6c221f0e12833c45d110
Javier Zapata <zapatajavier34@gmail.com>
2025-11-16 19:20:44 -0500

    add responseModel and crud functions

--- Convocatoria/src/main/java/com/example/demo/controllers/TeacherController.java
+++ Convocatoria/src/main/java/com/example/demo/controllers/TeacherController.java
@@ -1,9 +1,20 @@
 package com.example.demo.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.models.TeacherModel;
+import com.example.demo.models.response.ResponseModel;
 import com.example.demo.service.TeacherService;
 
 @RestController
@@ -12,5 +23,28 @@ public class TeacherController {
 	
 	@Autowired
 	TeacherService teacherService;
+	
+	@GetMapping
+	public ResponseEntity<List<TeacherModel>> getAll(){
+		return ResponseEntity.ok(teacherService.getAll());
+	}
+	
+	@PostMapping
+	public ResponseEntity<TeacherModel> 
+			save(@RequestBody TeacherModel teacherModel) {
+		return ResponseEntity.ok(teacherService.save(teacherModel));
+	}
+	
+	@PutMapping
+	public ResponseEntity<ResponseModel> 
+			update(@RequestBody TeacherModel teacherModel) {
+		return ResponseEntity.ok(teacherService.update(teacherModel));
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<ResponseModel>
+			delete(@PathVariable("id") int id) {
+		return ResponseEntity.ok(teacherService.delete(id));
+	}
 	
 }
--- Convocatoria/src/main/java/com/example/demo/service/TeacherService.java
+++ Convocatoria/src/main/java/com/example/demo/service/TeacherService.java
@@ -1,8 +1,12 @@
 package com.example.demo.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.models.TeacherModel;
+import com.example.demo.models.response.ResponseModel;
 import com.example.demo.repositories.ITeacherRepository;
 
 @Service
@@ -10,5 +14,48 @@ public class TeacherService {
 	
 	@Autowired
 	ITeacherRepository teacherRepository;
+	
+	//GET
+	public List<TeacherModel> getAll() {
+		// El entre parentesis funciona como un parse para convertir.
+		return (List<TeacherModel>) teacherRepository.findAll();	
+	}
+	
+	//POST
+	public TeacherModel save(TeacherModel teacherModel) {
+		//Hacemos validación previa
+		//insert into () values()
+		return teacherRepository.save(teacherModel);
+	}
+	
+	public ResponseModel update(TeacherModel teacherModel) {
+		try {
+			if(!teacherExist(teacherModel.getId())) {
+				return new ResponseModel(false, "Docente no existe");
+			} else {
+				teacherRepository.save(teacherModel);
+				return new ResponseModel(true, "Docente actualizado con exito");
+			}
+		} catch(Exception ex) {
+			return new ResponseModel(false, ex.toString());
+		}
+	}
+	
+	public ResponseModel delete(int Id) {
+		try {
+			if(!teacherExist(Id)) {
+				return new ResponseModel(false, "Docente no existe");
+			} else {
+				teacherRepository.deleteById(Id);
+				return new ResponseModel(true, "Docente eliminado");
+			}
+		} catch(Exception ex) {
+			return new ResponseModel(false, ex.getMessage());
+		}
+	}
+	
+	private boolean teacherExist(int Id) {
+		return teacherRepository.findById(Id).isEmpty() ? false : true;
+	}
 	
 }
